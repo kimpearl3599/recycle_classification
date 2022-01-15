20217a22fa406a7a4dfde833472f0336c23b8a93
woody <highsky21c@gmail.com>
2022-01-15 18:29:40 +0900

    photo_and_js

--- static/capture.js
+++ static/capture.js
@@ -1,40 +1,10 @@
-<!doctype html>
-<html>
-<head>
-	<title>WebRTC: Still photo capture demo</title>
-	<meta charset='utf-8'>
-	<link rel="stylesheet" href="main.css" type="text/css" media="all">
-	<script src="capture.js">
-	</script>
-</head>
-<body>
-<div class="contentarea">
-  <div class="camera">
-    <video id="video">Video stream not available.</video>
-    <button id="startbutton">Take photo</button>
-  </div>
-  <canvas id="canvas">
-  </canvas>
-<!--  <div class="output">-->
-<!--    <img id="photo" alt="The screen capture will appear in this box.">-->
-<!--  </div>-->
-
-<script>
-
-    // function posting() {
-    //     let
-    // }
-
-
-
-
 (function() {
   // The width and height of the captured photo. We will set the
   // width to the value defined here, but the height will be
   // calculated based on the aspect ratio of the input stream.
 
-  var width = 320;    // We will scale the photo width to this
-  var height = 0;     // This will be computed based on the input stream
+  var width = 480;    // We will scale the photo width to this
+  var height = 360;     // This will be computed based on the input stream
 
   // |streaming| indicates whether or not we're currently streaming
   // video from the camera. Obviously, we start at false.
@@ -88,20 +58,20 @@
       ev.preventDefault();
     }, false);
 
-    clearphoto();
+    // clearphoto();
   }
 
   // Fill the photo with an indication that none has been
   // captured.
-
-  function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
+  //
+  // function clearphoto() {
+  //   var context = canvas.getContext('2d');
+  //   context.fillStyle = "#AAA";
+  //   context.fillRect(0, 0, canvas.width, canvas.height);
+  //
+  //   var data = canvas.toDataURL('image/png');
+  //   photo.setAttribute('src', data);
+  // }
 
   // Capture a photo by fetching the current contents of the video
   // and drawing it into a canvas, then converting that to a PNG
@@ -115,20 +85,50 @@
       canvas.width = width;
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
+      // console.log(context)
 
-      var data = canvas.toDataURL('image/png');
-      photo.setAttribute('src', data);
+      var url_data = canvas.toDataURL('image/png');
+      // photo.setAttribute('src', url_data);
+      dataURLtoFile(url_data, 'data')
+      console.log(dataURLtoFile(url_data, 'data'))
+      console.log(CheckImagefiles('data'))
     } else {
-      clearphoto();
+      // clearphoto();
     }
   }
+
+  function dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = window.atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {
+        type: mime
+    });
+}
+
+function CheckImagefiles(fileName) {
+    var result = false;
+    var ext = fileName.substring(fileName.lastIndexOf('.') + 1);
+    if(!ext){
+        return result;
+    }
+    var imgs = ['gif', 'jpg', 'jpeg', 'png', 'bmp' ,'ico', 'apng'];
+    ext = ext.toLocaleLowerCase();
+    imgs.forEach( function(element) {
+        if(ext == element){
+            result = true;
+        }
+    });
+    return result;
+}
+
 
   // Set up our event listener to run the startup process
   // once loading is complete.
   window.addEventListener('load', startup, false);
 })();
-</script>
-
-</div>
-</body>
-</html>
